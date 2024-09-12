fa555c73f01be87614b332e2f715c1cb4f1b0c77
Michael Gao <michaelgao8@gmail.com>
2024-09-12 17:18:24 -0400

    Add additional documentation to the compute_metrics function

--- ctseval/ctseval/__init__.py
+++ ctseval/ctseval/__init__.py
@@ -1,25 +1,32 @@
 from .utils import validate_trajectories_schema, calculate_average_precision
 from ._ctseval import compute_metrics_c
 
-def compute_metrics(trajectories, snooze_window, detection_window, verbosity=1):
+def compute_metrics(trajectories: list[dict], snooze_window: float, detection_window: float, verbosity=1) -> list[dict]:
     """
     Compute metrics for clinical time series predictions.
     
     Args:
-    trajectories (list): List of trajectory dictionaries.
-    snooze_window (float): The snooze window duration.
-    detection_window (float): The detection window duration.
+    trajectories: List of trajectory dictionaries. Further described in the validate_trajectories_schema function.
+    snooze_window (float): The snooze window duration in units of time.
+    detection_window (float): The detection window duration in units of time.
     verbosity (int, optional): Verbosity level. Defaults to 1.
     
     Returns:
-    results: List of dictionaries with episode-level and prediction-levelmetrics.
+    results: List of dictionaries with episode-level and prediction-level metrics.
+        Each dictionary has the following keys:
+            {
+                "threshold":,
+                "episode_tp",
+                "episode_fp",
+                "episode_tn",
+                "episode_fn",
+                "prediction_tp",
+                "prediction_fp"
+            }
+        The returned list should have 1 entry for each possible threshold encountered, along with 1 additional value to make sure that the last threshold is accounted for
     """
-    # Validate the trajectories
     validate_trajectories_schema(trajectories)
-    
-    # Compute metrics using the C function
     results = compute_metrics_c(trajectories, snooze_window, detection_window, verbosity)
-    
     return results
 
 __all__ = ['compute_metrics', 'validate_trajectories_schema', 'calculate_average_precision']
